31e878cea0ba8ee38415406b427ed2dc5ed16622
szymkorp <48699984+szymkorp@users.noreply.github.com>
2020-06-24 17:32:22 -0400

    Add files via upload
    stores the name of all the scenes

---
+++ SceneType.cs
@@ -0,0 +1,17 @@
+﻿
+namespace UnityCore
+{
+    namespace Scene
+    {
+        public enum SceneType
+        {
+          None,
+          Menu,
+          Benton00,
+          Benton01,
+          Benton02,
+          EndScene
+        }
+
+    }
+}
