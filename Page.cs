d38365c2a562df92dcd4e697779e0b11befd9b5a
szymkorp <48699984+szymkorp@users.noreply.github.com>
2020-06-24 17:15:50 -0400

    Add files via upload
    Creates a Page object

---
+++ Page.cs
@@ -0,0 +1,52 @@
+﻿
+using UnityEngine;
+
+namespace UnityCore
+ {
+    namespace Menu
+    {
+
+        public class Page : MonoBehaviour
+        {
+            public static readonly string FLAG_ON = "On";
+            public static readonly string FLAG_OFF = "Off";
+            public static readonly string FLAG_NONE = "None";
+
+            public PageType type;
+            public bool useAnimation = false;
+            public string targetState { get; private set;}
+            private bool m_IsOn;
+            
+
+            public bool isOn
+            {
+                get
+                {
+                    return m_IsOn;
+                }
+                private set
+                {
+                    m_IsOn = false;
+                }
+            }
+
+
+            #region Unity Functions
+
+            private void OnEnable()
+            {
+
+            }
+
+            #endregion
+            public void Animate(bool flag)
+            {
+                    gameObject.SetActive(false);
+            }
+
+
+
+        }
+    }
+  
+}
