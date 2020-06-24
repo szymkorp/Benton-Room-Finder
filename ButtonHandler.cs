e4f5b8f6978ab799d03dbce78e2ff92805a1ff75
szymkorp <48699984+szymkorp@users.noreply.github.com>
2020-06-24 17:42:19 -0400

    Add files via upload

---
+++ ButtonHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityCore.Data;
+using UnityCore.Scene;
+using UnityCore.Menu;
+
+
+public class ButtonHandler : MonoBehaviour
+{
+    public SceneController sceneController;
+    public DataPresistence DP;
+    public PageController menu;
+
+    public void nextScene()
+    {
+        int loadScene = DP.LoadScene;
+        if(loadScene == 0)
+        {
+            sceneController.Load(SceneType.Benton00);//, null, false, PageType.Loading);
+        }
+        if (loadScene == 1)
+        {
+            sceneController.Load(SceneType.Benton01);
+        }
+        if (loadScene == 2)
+        {
+            sceneController.Load(SceneType.Benton02);
+        }
+
+    }
+
+}
