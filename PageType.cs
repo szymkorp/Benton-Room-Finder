ba3b642f994713f2c0f317b6b822bc30081f8f35
szymkorp <48699984+szymkorp@users.noreply.github.com>
2020-06-24 17:11:38 -0400

    Add files via upload

---
+++ PageType.cs
@@ -0,0 +1,13 @@
+﻿namespace UnityCore
+{
+    namespace Menu
+    {
+        public enum PageType
+        {
+            None,
+            Loading,
+            Menu,
+            end
+        }
+    }
+}
